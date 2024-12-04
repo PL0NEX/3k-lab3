f88e5bc505be3315110a567b4124d4bb62a88339
PL0NEX <164649801+PL0NEX@users.noreply.github.com>
2024-12-04 15:13:07 +0300

    Update PeriodController.cs

--- MyWebApi/Controllers/PeriodController.cs
+++ MyWebApi/Controllers/PeriodController.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GoalsExample.WebApi.Controllers
+namespace MyWebApi.Controllers
 {
     [ApiController]
     [Route("period")]
